93769119c2cd435ae2a755272dafc45671372767
Aswin Sreeraj <30628448+aswinsreeraj@users.noreply.github.com>
2026-09-10 22:27:19 +0530

    Time: 8 ms (16.67%), Space: 8.7 MB (33.33%) - LeetHub

--- 3895-count-digit-appearances/3895-count-digit-appearances.go
+++ 3895-count-digit-appearances/3895-count-digit-appearances.go
@@ -1,11 +1,7 @@
 func countDigitOccurrences(nums []int, digit int) int {
     count := 0
     for _, val := range nums {
-        for val > 0 {
-            valdig := val % 10
-            if valdig == digit {count++}
-            val /= 10
-        }
+        count += strings.Count(strconv.Itoa(val), strconv.Itoa(digit))
     }
     return count
 }
